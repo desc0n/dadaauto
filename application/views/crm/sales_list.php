f43b4f08b9800a90d50c28302e0c7fe8874dd440
desc0n <desconbk@yandex.ru>
2016-06-30 20:12:49 +1000

    Редактирую вывод списка реализаций

--- application/views/crm/sales_list.php
+++ application/views/crm/sales_list.php
@@ -33,44 +33,29 @@
                 </div><!-- /.col-lg-2 -->
             </div><!-- /.row -->
         </form>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                Список событий
-            </div>
-            <!-- /.panel-heading -->
-            <div class="panel-body">
-                <div class="dataTable_wrapper">
-                    <table class="table table-striped table-bordered table-hover actions-table" id="dataTables-example">
-                        <thead>
-                        <tr>
-                            <th>№ реализации</th>
-                            <th>Дата реализации</th>
-                            <th>Сумма реализации</th>
-                            <th>Действия</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?foreach ($actionsList as $action) {?>
-                            <tr>
-                                <td class="text-center"><?=$action['id'];?></td>
-                                <td class="text-center"><?=date('H:i d.m.Y', strtotime($action['date']));?></td>
-                                <td><?=$action['sale_price'];?></td>
-                                <td class="text-center">
-                                    <a class="btn btn-default" href="/crm/sale/<?=$action['id'];?>"><i class="fa fa-search fa-fw"></i></a>
-                                </td>
-                            </tr>
-                        <?}?>
-                        </tbody>
-                    </table>
-                </div>
-                <!-- /.table-responsive -->
-            </div>
-            <!-- /.panel-body -->
+        <div class="dataTable_wrapper">
+            <table class="table table-striped table-bordered table-hover actions-table" id="dataTables-example">
+                <thead>
+                <tr>
+                    <th>№ реализации</th>
+                    <th>Дата реализации</th>
+                    <th>Сумма реализации</th>
+                    <th>Действия</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?foreach ($actionsList as $action) {?>
+                    <tr>
+                        <td class="text-center"><?=$action['id'];?></td>
+                        <td class="text-center"><?=date('H:i d.m.Y', strtotime($action['date']));?></td>
+                        <td><?=$action['sale_price'];?></td>
+                        <td class="text-center">
+                            <a class="btn btn-default" href="/crm/sale/<?=$action['id'];?>"><i class="fa fa-search fa-fw"></i></a>
+                        </td>
+                    </tr>
+                <?}?>
+                </tbody>
+            </table>
         </div>
-        <div class="form-group">
-            <button class="btn btn-success" data-toggle="modal" data-target="#addActionModal">Добавить событие <i class="fa fa-plus fa-fw"></i></button>
-        </div>
-        <!-- /.panel -->
     </div>
-    <!-- /.col-lg-12 -->
 </div>
