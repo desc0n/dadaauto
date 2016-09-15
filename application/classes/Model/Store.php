01b715888a07a451ce8c027faad09f778075448b
desc0n <desconbk@yandex.ru>
2016-09-15 21:59:28 +1000

    Добавляю проверку на наличие имени файла при обновлении картинок

--- application/classes/Model/Store.php
+++ application/classes/Model/Store.php
@@ -594,7 +594,7 @@ class Model_Store extends Kohana_Model
     {
         $file_name = 'public/img/original/'.$id.'.jpg';
         
-        if (copy($url, $file_name))	{
+        if (!empty($file_name) && copy($url, $file_name))	{
             $watermark = Image::factory('public/i/watermark.jpg');
 
             $image = Image::factory($file_name);
